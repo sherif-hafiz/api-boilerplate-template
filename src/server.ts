f73682eb5901ce18411e68595cb0d0eed5bcfd84
Sherif Hafiz <sherif.hafiz@gmail.com>
2019-11-01 23:13:15 +0000

    Updates server.ts with dependency

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import http from "http";
 import express from "express";
+import { applyMiddleware, applyRoutes } from "./utils";
+import routes from "./services";
+import middleware from "./middleware";
 
 const router = express();
+applyMiddleware(middleware, router);
+applyRoutes(routes, router);
+
 const { PORT = 3000 } = process.env;
 const server = http.createServer(router);
 
